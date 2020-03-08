db1b3568c465a13d1f3af8dc316a5c4e0fa1285e
1199692 <56451987+1199692@users.noreply.github.com>
2020-03-09 02:11:12 +0530

    Update BuiltOn_Test_Question1_Highest_Product.py

--- BuiltOn_Test_Question1_Highest_Product.py
+++ BuiltOn_Test_Question1_Highest_Product.py
@@ -1,3 +1,4 @@
+#1.  Create a method that, given a list of integers, returns the highest product between 3 of those numbers. Example:  [1, 10, 2, 6, 5, 3] = 300
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 26 07:29:43 2020
